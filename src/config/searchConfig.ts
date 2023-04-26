e31dcd28503432f12f94de12226c0a0975309418
Justinrich8 <109759966+Justinrich8@users.noreply.github.com>
2023-04-26 09:48:05 -0400

    Add files via upload

---
+++ src/config/searchConfig.ts
@@ -0,0 +1,7 @@
+export const config = {
+  apiKey: "c1e993cb286cd238fcf4f43f57e686ff",
+  experienceKey: "us_presidents_demo",
+  locale: "en",
+  experienceVersion: "PRODUCTION",
+  businessId: 3155537,
+};
